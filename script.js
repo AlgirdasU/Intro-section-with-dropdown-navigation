6c37e7adaaf40d1720f0a6844ddaa2bac07c4fac
algirdas.urbonavicius <algirdas.urbonavicius@gmail.com>
2022-07-25 17:24:28 +0300

    Added all features

--- script.js
+++ script.js
@@ -1,93 +1,89 @@
 "use strict";
 
-const nav = document.querySelector(".menu-nav");
-const navList = document.querySelector(".menu-nav__list");
-const navItem = document.querySelector(".menu-nav__item");
 const navItemHover = document.querySelectorAll(".menu-nav__item--hover");
-const navLink = document.querySelector(".menu-nav__link");
-// const navIcon = document.querySelectorAll(".menu-nav__icon");
 const dropdown = document.querySelectorAll(".dropdown");
 const dropdownList = document.querySelector(".dropdown__list");
-// const navIconUp = document.querySelector("menu-nav__icon--up");
-// const navIconDown = document.querySelector("menu-nav__icon--down");
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
-
-document.addEventListener("mouseover", () => {
-  if (screen.width < 900) {
-    navItemHover.forEach(
-      (hover) => hover.classList.remove("menu-nav__item--hover")
-      // console.log(hover.classList.remove("menu-nav__item--hover"))
-    );
-  }
-  if (screen.width > 900) {
-    navItemHover.forEach((hover) =>
-      hover.classList.add("menu-nav__item--hover")
-    );
-  }
-});
-
-btnNavEl.addEventListener("click", () => {
-  headerEl.classList.toggle("nav-open");
-});
+const mobileBc = document.querySelector(".mobile-bc");
+const navIcon = document.querySelectorAll(".menu-nav__icon");
 
 document.addEventListener("click", function (e) {
-  if (screen.width < 900) {
+  if (window.innerWidth < 900) {
     const clickedDropdownButton = e.target.closest("[data-btn]");
-    // 1. if no clicked on the ddButton && clicked inside dropdown, then return (do nothing).
     if (!clickedDropdownButton && e.target.closest(".dropdown") != null) return;
-    // const t = clickedDropdownButton;
-    // console.log(t);
-
-    // const test = clickedDropdownButton.querySelectorAll(".menu-nav__icon");
-    // console.log(test);
 
     let currentDropdown;
-    let currentDropdownList; // Mobile 00
+    let currentDropdownList;
     let currentIcon;
-    // 2. If we clicked ddButton, currentDropdown is equal dropdown.
     if (clickedDropdownButton) {
       currentDropdown = document.querySelector(
         `.dropdown__${clickedDropdownButton.dataset.btn}`
       );
 
-      // mobile 01
       currentDropdownList = document.querySelector(
         `.dropdown__list--${clickedDropdownButton.dataset.btn}`
       );
 
-      // ICON rotate
       currentIcon = document.querySelector(
         `.menu-nav__icon--${clickedDropdownButton.dataset.btn}`
       );
 
-      // 3. If dropdown is closed(hidden), then we add(toggle) class="dropdown__visible" and vice versa
+      // If dropdown is closed(hidden), then we add(toggle) class="dropdown__visible" and vice versa
       currentDropdown.classList.toggle("dropdown__visible");
-      currentDropdownList.classList.toggle("dropdown__list--active"); // mobile 02
-      // ICON
+      currentDropdownList.classList.toggle("dropdown__list--active");
       currentIcon.classList.toggle("menu-nav__icon--rotate");
     }
 
-    document.querySelectorAll(".menu-nav__icon--rotate").forEach((icon) => {
-      if (currentIcon === icon) return;
-      icon.classList.remove("menu-nav__icon--rotate");
+    // If you'll hide this code from HERE to 55 line you can open all sub-menu at the same time.
+    // Menu arrow btn's are rotated.
+    document.querySelectorAll(".menu-nav__icon--rotate").forEach((rotate) => {
+      if (currentIcon === rotate) return;
+      rotate.classList.remove("menu-nav__icon--rotate");
     });
-    // ICON
-
     // we loop over all opened dropdown and close them exept the one that we just opened.
     document.querySelectorAll(".dropdown__visible").forEach((dropdown) => {
-      // if (dropdown === currentDropdown) return;
-      // dropdown.classList.remove("dropdown__visible");
       currentDropdown === dropdown
         ? "return"
         : dropdown.classList.remove("dropdown__visible");
     });
-
     // The Position: absolute is changed to relative
     document.querySelectorAll(".dropdown__list--active").forEach((active) => {
       if (currentDropdownList === active) return;
       active.classList.remove("dropdown__list--active");
     });
-    ///////////// active
+    // TO HERE
   }
+});
+
+window.onload = resetMenu();
+function resetMenu() {
+  if (window.innerWidth <= 900) {
+    navItemHover.forEach((hover) =>
+      hover.classList.remove("menu-nav__item--hover")
+    );
+  }
+  if (window.innerWidth > 900) {
+    navItemHover.forEach((hover) =>
+      hover.classList.add("menu-nav__item--hover")
+    );
+
+    document
+      .querySelectorAll(".menu-nav__icon--rotate")
+      .forEach((icon) => icon.classList.remove("menu-nav__icon--rotate"));
+    document
+      .querySelectorAll(".dropdown__list--active")
+      .forEach((ddList) => ddList.classList.remove("dropdown__list--active"));
+
+    document
+      .querySelectorAll(".dropdown__visible")
+      .forEach((visible) => visible.classList.remove("dropdown__visible"));
+  }
+}
+window.addEventListener("resize", resetMenu);
+
+// We switch between menu/close btn and add transparent dark grey bc.
+btnNavEl.addEventListener("click", () => {
+  headerEl.classList.toggle("nav-open");
+  mobileBc.classList.toggle("transparent-mobile-bc");
 });
